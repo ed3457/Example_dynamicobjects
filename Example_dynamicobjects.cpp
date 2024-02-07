d27eee200bcd487ce4d9ee52e2fba69e2e4b32c4
Kamel R <kamel.r@wayne.edu>
2024-02-07 18:50:59 -0500

    created an array of pointers

--- Example_dynamicobjects.cpp
+++ Example_dynamicobjects.cpp
@@ -6,7 +6,29 @@
 using namespace std; 
 int main()
 {
- 
+	const int count = 4;
+	Wheel wheels[count]; 
+
+	int noWheels = 6;
+
+	Wheel* setOfWheels = new Wheel[noWheels];
+
+	//for (int i = 0; i < noWheels; i++)
+	//	setOfWheels[i].printType();
+
+
+	//delete[] setOfWheels;
+
+	//setOfWheels = new Wheel[4];
+
+	int size2 = 4; 
+	Wheel** setOfWheels2 = new Wheel * [size2];
+
+	for (int i = 0; i < size2; i++)
+		setOfWheels2[i] = new Wheel();
+
+	for (int i = 0; i < size2; i++)
+		setOfWheels2[i]->printType();
 
 }
 
